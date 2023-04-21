193a76872b14423108b3e9046b5e0da36c4ca349
alumnosmmra <alumnosmmra@gmail.com>
2023-04-21 00:58:56 -0600

    Initial commit

---
+++ js/scritp.js
@@ -0,0 +1,7 @@
+const menu = document.getElementById('menu');
+const details = document.getElementById('details');
+const mesas = document.getElementsByClassName('mesa');
+
+const mostrarOpciones = (id) => {
+    console.log(id);
+}
